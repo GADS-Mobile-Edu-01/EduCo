9de5cf89ec64360e2e8a6235ba35c28e4ff69383
codose <osemwingieoshodin@gmail.com>
2020-10-07 13:56:37 +0100

    Updated Colors, Fonts and added required vectors

--- app/src/main/java/com/android/educo/views/main/MainActivity.kt
+++ app/src/main/java/com/android/educo/views/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.android.educo
+package com.android.educo.views.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.educo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
