31b33937ea80ac04805284764faa8edf65e16489
Abdurrahman Lawal Zailani <devpsys@gmail.com>
2020-10-08 21:03:50 +0100

    login screen ui implemented

---
+++ app/src/main/java/com/android/educo/views/main/ui/activities/LoginActivity.kt
@@ -0,0 +1,62 @@
+package com.android.educo.views.main.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.android.educo.R
+import com.android.educo.databinding.ActivityLoginBinding
+import com.android.educo.views.main.utils.isValidEmail
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        init()
+    }
+
+    private fun init() {
+        mBinding.tvForgotPassword.setOnClickListener {
+            //  TODO: Add the code to navigate to Forget Password Screen.
+        }
+        mBinding.btnSignIn.setOnClickListener { signUp() }
+        mBinding.btnSignUp.setOnClickListener {
+            //  TODO: Add the code to navigate to Sign Up Screen.
+        }
+    }
+
+    private fun signUp() {
+        val email = mBinding.edtEmail.text.toString().trim()
+        val password = mBinding.edtPassword.text.toString().trim()
+
+        if (validateFields(email, password)) {
+            //  TODO: Handle the login process here.
+        }
+    }
+
+    /**
+     * This function validates form fields.
+     *
+     * @param email
+     * @param password
+     *
+     * @return true if the form is valid or false if otherwise.
+     */
+    private fun validateFields(email: String, password: String): Boolean {
+        if (!email.isValidEmail()) {
+            mBinding.edtEmail.error = getString(R.string.error_message_in_valid_email)
+            return false
+        }
+
+        if (password.length < 8) {
+            mBinding.edtPassword.error = getString(R.string.error_message_short_password)
+            return false
+        }
+
+        return true
+    }
+}
