2f0e5fb60af50484e9d7eb945f745eb1146de99a
codose <osemwingieoshodin@gmail.com>
2020-10-09 14:04:23 +0100

    Added bottom navigation bar and fixed some errors in strings.xml and colors.xml

--- app/src/main/java/com/android/educo/views/main/MainActivity.kt
+++ app/src/main/java/com/android/educo/views/main/MainActivity.kt
@@ -2,11 +2,18 @@ package com.android.educo.views.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.android.educo.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = this.findNavController(R.id.navHostFragment)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
