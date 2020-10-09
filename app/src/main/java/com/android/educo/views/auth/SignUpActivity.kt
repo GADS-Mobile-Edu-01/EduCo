1c9a5b15ca65475d390a0ea923a8b7c0d433c04b
Christian-Oj <42995252+Christian-Oj@users.noreply.github.com>
2020-10-09 11:06:48 +0100

    Ft sign up (#13)
    * I have completed the sign up page
    
    * I have completed the sign up page
    
    * I have completed the sign up page

---
+++ app/src/main/java/com/android/educo/views/auth/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.android.educo.views.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.educo.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
