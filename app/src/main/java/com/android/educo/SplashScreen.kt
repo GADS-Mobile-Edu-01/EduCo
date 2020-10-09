81ee33d85f4eee52fbe86daf04b0a667a31a38b9
taofeeq23 <50840385+taofeeq23@users.noreply.github.com>
2020-10-09 16:59:47 +0100

    SplashScreen created

---
+++ app/src/main/java/com/android/educo/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.android.educo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.android.educo.views.auth.WelcomeActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent (this@SplashScreen, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
