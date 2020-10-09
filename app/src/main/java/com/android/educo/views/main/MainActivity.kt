0dc424c6dbac4c0e45bb4db4ae7004378d287b98
christianojiezele@gmail.com <christianojiezele@gmail.com>
2020-10-09 09:48:31 +0100

    I have completed the sign up page

--- app/src/main/java/com/android/educo/views/main/MainActivity.kt
+++ app/src/main/java/com/android/educo/views/main/MainActivity.kt
@@ -7,6 +7,6 @@ import com.android.educo.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/android/educo/views/main/authentication/sign_up.kt
+++ app/src/main/java/com/android/educo/views/main/authentication/sign_up.kt
@@ -7,6 +7,6 @@ import com.android.educo.R
 class sign_up : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 }
